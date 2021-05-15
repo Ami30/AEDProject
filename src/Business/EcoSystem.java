3a88184ae360de181c4c9928e5119aabc897ce36
Ami30 <gupta.ami@northeastern.edu>
2021-05-15 15:28:06 -0400

    Added basic packages

--- src/Business/EcoSystem.java
+++ src/Business/EcoSystem.java
@@ -2,13 +2,55 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- */
-package Business;
+ */package Business;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author amishagupta
  */
 public class EcoSystem {
+    private static EcoSystem business;
+   // private ArrayList<Network> networkList;
+    public static EcoSystem getInstance(){
+        if(business==null){
+            business=new EcoSystem();
+        }
+        return business;
+    }
     
+//    public Network createAndAddNetwork(){
+//        Network network=new Network();
+//        networkList.add(network);
+//        return network;
+//    }
+//    @Override
+//    public ArrayList<Role> getSupportedRole() {
+//        ArrayList<Role> roleList=new ArrayList<Role>();
+//        roleList.add(new Admin());
+//        return roleList;
+//    }
+//    public Ecosystem(){
+//        super(null);
+//        networkList=new ArrayList<Network>();
+//    }
+//
+//    public ArrayList<Network> getNetworkList() {
+//        return networkList;
+//    }
+//
+//    public void setNetworkList(ArrayList<Network> networkList) {
+//        this.networkList = networkList;
+//    }
+//    
+//    public boolean checkIfUserIsUnique(String userName){
+//        if(!this.getUserDir().checkIfUsernameIsUnique(userName)){
+//            return false;
+//        }
+//        for(Network network:networkList){
+//            
+//        }
+//        return true;
+//    }
 }
